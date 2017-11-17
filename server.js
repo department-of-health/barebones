cb487038098c4c75792814c233b10e7cf2dac3c9
Demotive <mat@demotive.com>
2017-11-17 16:21:29 +0000

    No robots, no indexing

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const nunjucks = require('nunjucks')
 
 const app = express()
 
-// Middleware to serve static assets
-app.use('/', express.static(path.join(__dirname, '/public')))
-
 // Application settings
 app.set('view engine', 'html')
 
@@ -16,6 +13,22 @@ var env = nunjucks.configure('./app/views', {
     noCache: true
 });
 
+// Middleware to serve static assets
+app.use('/', express.static(path.join(__dirname, '/public')))
+
+// Disallow search index
+app.use(function (req, res, next) {
+  // Setting headers stops pages being indexed even if indexed pages link to them.
+  res.setHeader('X-Robots-Tag', 'noindex')
+  next()
+})
+
 app.get('/', (req, res) => res.render('index.html'))
+
+// No robots plz
+app.get('/robots.txt', function (req, res) {
+  res.type('text/plain')
+  res.send('User-agent: *\nDisallow: /')
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
