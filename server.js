1282375550f04f05e654a25a7081f99b87646d8c
Demotive <mat@demotive.com>
2017-11-27 14:56:41 +0000

    Use session data in form and display

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
-var express = require('express')
 var path = require('path')
+var express = require('express')
+var session = require('express-session')
+var bodyParser = require('body-parser')
 var nunjucks = require('nunjucks')
+var moment = require('moment')
 var dateFilter = require('nunjucks-date-filter')
 var config = require('./config.js')
 var utils = require('./lib/utils.js')
 
 var app = express()
+
+//moment().format()
 
 // Application settings
 app.set('view engine', 'html')
@@ -15,7 +20,7 @@ var env = nunjucks.configure('./app/views', {
     express: app,
     noCache: true
 })
-env.addFilter('date', dateFilter);
+env.addFilter('date', dateFilter)
 
 // Middleware to serve static assets
 app.use('/', express.static(path.join(__dirname, '/public')))
@@ -27,19 +32,77 @@ app.use(function (req, res, next) {
   next()
 })
 
+// Support session data
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: Math.round(Math.random() * 100000).toString()
+}))
+
 // Add variables that are available in all views
 app.use(function (req, res, next) {
-  res.locals.vars = config.vars;
-  res.locals.jsNow = new Date();
+  res.locals.serviceName = config.serviceName
+  res.locals.jsNow = moment().format()
   next()
 })
 
+var myLogger = function (req, res, next) {
+  console.log(req.session);
+  next()
+}
+app.use(myLogger)
+
+// Handle form POSTS
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}))
+
 app.get('/', (req, res) => res.render('index.html'))
+
+app.get('/details-deceased', function(req, res) {
+  res.render('details-deceased.html', {
+    session: req.session
+  })
+})
+app.get('/edit-deceased', function(req, res) {
+  res.render('edit-deceased.html', {
+    session: req.session
+  })
+})
+app.post('/edit-deceased', function(req, res) {
+  req.session.deceased = req.body
+  console.log(req.body)
+  // formatting and calculations
+  // is the dod (at least) present?
+  if (req.body['dod-year'] !== '' && req.body['dod-month'] !== '' && req.body['dod-day'] !== '') {
+    if (req.body['tod-hour'] !== '' && req.body['tod-mins'] !== '') {
+      var timeStr = ' ' + req.body['tod-hour'] + ':' + req.body['tod-mins'] + ':00'
+    }
+    var death = moment(
+      req.body['dod-year'] + '-' +
+      req.body['dod-month'] + '-' +
+      req.body['dod-day'] +
+      timeStr
+    )
+    req.session.deceased.deathTimestamp = death
+  }
+  // is dob present?
+  if (req.body['dob-year'] !== '' && req.body['dob-month'] !== '' && req.body['dob-day'] !== '') {
+    var birth = moment(req.body['dob-year'] + '-' + req.body['dob-month'] + '-' + req.body['dob-day'])
+    req.session.deceased.birth = birth
+  }
+  if (typeof death !== 'undefined' && typeof birth !== 'undefined' ) {
+    var age = death.diff(birth, 'years')
+    req.session.deceased.age = age
+  }
+  res.redirect('/details-deceased');
+})
 
 // auto render any view that exists
 app.get(/^\/([^.]+)$/, function (req, res) {
   var path = (req.params[0])
-  res.render(path, function (err, html) {
+  res.render(path, { session: req.session }, function (err, html) {
     if (err) {
       res.render(path + '/index', function (err2, html) {
         if (err2) {
