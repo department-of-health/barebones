1d6f40ddd743d609b193ac5a799d1c87baa64452
Demotive <mat@demotive.com>
2017-12-01 16:54:59 +0000

    Add ability to rename case

--- config.js
+++ config.js
@@ -2,6 +2,7 @@
 // Note: prototype config can be overridden using environment variables (eg on heroku)
 module.exports = {
   serviceName: 'Medical Examiner Service',
+  caseRef: 'ME000001',
   // Default port that prototype runs on
   port: '3000',
   // Enable or disable password protection on production
